a9f2ffb7ceab8fadd3d63e1dfa72639ecdcd2821
pipopotamasu <ec0156hx@gmail.com>
2018-02-15 12:07:33 +0900

    pug version

---
+++ app/javascript/components/chapter2/PugTodoApp.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  div#pug-todo-app
+    input(v-model="input" type="text" size="20")
+    button(@click="addTodo") Submit
+    ul
+      template(v-for="(todo, i) in todos")
+        li
+          input(v-model="todo.checked" type="checkbox" :id="'todo-' + i")
+          label(:class="{ done: todo.checked }" :for="'todo-' + i") {{ todo.body }}
+          span(@click="deleteTodo(i)" class="delete") [×]
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      input: '',
+      todos: [ { body: 'サンプルだよ!', checked: false } ]
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if(this.input == '') return false;
+      this.todos.push({ body: this.input, checked: false })
+      this.input = '';
+    },
+    deleteTodo: function(index) {
+      console.log('Deleted!')
+    }
+  }
+}
+</script>
--- app/javascript/packs/chapter2/index.js
+++ app/javascript/packs/chapter2/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import TodoApp from '../../components/chapter2/TodoApp.vue'
+import PugTodoApp from '../../components/chapter2/PugTodoApp.vue'
 
 Vue.component('todo-app', TodoApp)
+Vue.component('pug-todo-app', PugTodoApp)
 
 new Vue({
   el: '#app'
