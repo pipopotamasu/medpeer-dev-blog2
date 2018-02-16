eff0e51e1e1c9eed87bfbabb31c10844e554d959
pipopotamasu <ec0156hx@gmail.com>
2018-02-16 11:48:59 +0900

    add hinagata

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/chapter1', to: 'chapter1#index'
   get '/chapter2', to: 'chapter2#index'
+  get '/chapter3', to: 'chapter3#new'
   post '/users', to: 'users#create'
   root to: 'home#index'
 end
