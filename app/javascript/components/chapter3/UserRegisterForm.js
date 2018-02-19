7e4095c414fb91a8531d8ece0b48ff9d24a4a3a1
pipopotamasu <ec0156hx@gmail.com>
2018-02-19 19:52:46 +0900

    validate

--- app/javascript/components/chapter3/UserRegisterForm.js
+++ app/javascript/components/chapter3/UserRegisterForm.js
@@ -11,12 +11,29 @@ export default {
   methods: {
       onSubmit: function(e) {
         e.preventDefault()
+        this.errors = []
         if(this.valid()) {
           // submit!
         }
       },
       valid: function() {
+        this.validateLastName()
+        this.validateFirstName()
+        this.validateAge()
+        this.validateEmail()
         return this.errors.length === 0
-      }
+      },
+      validateLastName: function() {
+        if(this.lastname.length == 0) this.errors.push({ key: 'lastname', message: '苗字を入力してください'})
+      },
+      validateFirstName: function() {
+        if(this.firstname.length == 0) this.errors.push({ key: 'firstname', message: '名前を入力してください'})
+      },
+      validateAge: function() {
+        if(this.age <= 0) this.errors.push({ key: 'age', message: '年齢を正しく入力してください' })
+      },
+      validateEmail: function() {
+        if(this.email == '') this.errors.push({ key: 'email', message: 'メールアドレスを入力してください' })
+      },
   }
 }
