aef0f2aecdaa3a614fea167ff8290cd6118236ac
pipopotamasu <ec0156hx@gmail.com>
2018-02-19 12:58:22 +0900

    add inline template

---
+++ app/javascript/components/chapter3/UserRegisterForm.js
@@ -0,0 +1,11 @@
+export default {
+  data: function () {
+    return {
+      errors: ['hoge', 'fuga'],
+      lastname: ''
+    }
+  },
+  created: function() {
+    console.log(1)
+  }
+}
--- app/javascript/packs/chapter3/new.js
+++ app/javascript/packs/chapter3/new.js
@@ -1,1 +1,8 @@
-console.log('new')
+import Vue from 'vue'
+import UserRegisterForm from '../../components/chapter3/UserRegisterForm'
+
+Vue.component('user-register-form', UserRegisterForm)
+
+new Vue({
+  el: '#user-register-form'
+})
